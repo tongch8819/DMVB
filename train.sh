d914b3017b72fab2c56631bf3eeb840456bb3c4f
gcr-train <gcr-train@microsoft.com>
2022-10-08 14:20:58 +0000

    improve train.sh

--- train.sh
+++ train.sh
@@ -1,14 +1,16 @@
+domain_name="finger"
+task_name="spin"
 date_str=$(date +"%Y-%m-%d")
 python train.py \
     --agent bmv \
-    --domain_name cheetah \
-    --task_name run \
+    --domain_name $domain_name \
+    --task_name $task_name \
     --encoder_type pixel \
     --decoder_type identity \
     --action_repeat 4 \
     --save_video \
     --save_tb \
-    --work_dir ./exp/cheetah_run_$date_str \
+    --work_dir ./exp/${domain_name}_${task_name}_${date_str} \
     --seed 1 \
     --crop_size 68 \
     1>exp/train.log 2>&1 &
